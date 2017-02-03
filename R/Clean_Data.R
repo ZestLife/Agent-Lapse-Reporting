2f4a4ea77af9283ce73f075d0af293ff1c6df2d1
Jadon Thomson <jadon.thomson@zestlife.co.za>
2017-02-03 11:56:17 +0200

    Cleaning data, importing data and creating the report. Copied from the GAP version and adapted it to the Assetlife stuff.

---
+++ R/Clean_Data.R
@@ -0,0 +1,87 @@
+# Clean data for lapse report
+
+
+# Use ID to find gender
+gggg    <- as.numeric(substr(All_Data$POLICYHOLDERIDNUMBER, 7,10))
+gggg[nchar(as.character(All_Data$POLICYHOLDERIDNUMBER)) != 13] <- NA
+gggg[gggg < 5000] <- "F"
+gggg[gggg >= 5000] <- "M"
+
+# Calculate start Age
+All_Data$COMMENCEMENTDATEOFPOLICY <- DateConv(All_Data$COMMENCEMENTDATEOFPOLICY)
+All_Data$STARTAGE <- year(All_Data$COMMENCEMENTDATEOFPOLICY) - year(All_Data$BDATE)
+All_Data$STARTAGE[All_Data$STARTAGE < 18] <- NA
+
+# Remove unnecessary columns
+Columns <- c("UNIQUEID",
+             "AFFINITYGROUP",
+             "PRODUCTCODE",
+             "POLICYNUMBER",
+             "POLICYHOLDERIDNUMBER",
+             "COMMENCEMENTDATEOFPOLICY",
+             "STATUS",
+             "STATUSEFFECTIVEENDDATE",
+             "AGENTNAME",
+             "GENDER",
+             "ACTIVEPREMIUM",
+             "TOTALAMOUNTPAIDSINCEINCEPTIONTOCURRENTMONTH")
+
+All_Data <- subset(All_Data, select = Columns)
+
+# Clean Premiums paid
+All_Data$TOTALAMOUNTPAIDSINCEINCEPTIONTOCURRENTMONTH <- gsub(" ","",All_Data$TOTALAMOUNTPAIDSINCEINCEPTIONTOCURRENTMONTH)
+All_Data$TOTALAMOUNTPAIDSINCEINCEPTIONTOCURRENTMONTH <- as.numeric(All_Data$TOTALAMOUNTPAIDSINCEINCEPTIONTOCURRENTMONTH)
+All_Data$TOTALAMOUNTPAIDSINCEINCEPTIONTOCURRENTMONTH[is.na(All_Data$TOTALAMOUNTPAIDSINCEINCEPTIONTOCURRENTMONTH)] = 0
+
+# Clean status
+All_Data$STATUS[All_Data$TOTALAMOUNTPAIDSINCEINCEPTIONTOCURRENTMONTH == 0] <- "NTU"
+All_Data$STATUS[All_Data$TOTALAMOUNTPAIDSINCEINCEPTIONTOCURRENTMONTH != 0 & All_Data$STATUS == "NTU"] <- "LAP"
+
+
+# Clean effective end date
+All_Data$STATUSEFFECTIVEENDDATE <- DateConv(All_Data$STATUSEFFECTIVEENDDATE)
+
+
+# Clean affinity group code
+All_Data$AFFINITYGROUP <- as.character(All_Data$AFFINITYGROUP)
+string <- All_Data$AFFINITYGROUP[str_sub(All_Data$AFFINITYGROUP, start = -1) == " "]
+string <- substr(string, 1, nchar(string) - 1)
+All_Data$AFFINITYGROUP[str_sub(All_Data$AFFINITYGROUP, start = -1) == " "] <- string
+
+# New End Date
+All_Data$NEW_END_DATE <- All_Data$STATUSEFFECTIVEENDDATE
+All_Data$NEW_END_DATE[All_Data$STATUS == "ACT"] <- as.Date(today(tzone = ""))
+All_Data$NEW_END_DATE[All_Data$STATUS == "NTU"] <- All_Data$COMMENCEMENTDATE[All_Data$STATUS == "NTU"] 
+date_errors <- All_Data$NEW_END_DATE[All_Data$STATUS == "LAP" & day(All_Data$NEW_END_DATE) == 1]
+date_errors[month(date_errors) == 12] <- date_errors[month(date_errors) == 12] + 30 
+date_errors[month(date_errors) != 12] <- as.Date(paste0(year(date_errors[month(date_errors) != 12]),"-", 
+                                                        month(date_errors[month(date_errors) != 12]) + 1,"-", 
+                                                        day(date_errors[month(date_errors) != 12]))) - 1
+All_Data$NEW_END_DATE[All_Data$STATUS == "LAP" & day(All_Data$NEW_END_DATE) == 1] <- date_errors
+
+# 
+# # Premium data
+# All_Data$OLDMONTHLYPREMIUMAMOUNTTOENDJAN2010 <- as.numeric(gsub(" ","",All_Data$OLDMONTHLYPREMIUMAMOUNTTOENDJAN2010)) 
+# All_Data$OLDMONTHLYPREMIUMAMOUNTTOENDJAN2010[is.na(All_Data$OLDMONTHLYPREMIUMAMOUNTTOENDJAN2010)] = 0
+# All_Data$OLDMONTHLYPREMIUMAMOUNTTOENDJAN2011 <- as.numeric(gsub(" ","",All_Data$OLDMONTHLYPREMIUMAMOUNTTOENDJAN2011)) 
+# All_Data$OLDMONTHLYPREMIUMAMOUNTTOENDJAN2011[is.na(All_Data$OLDMONTHLYPREMIUMAMOUNTTOENDJAN2011)] = 0
+# All_Data$OLDMONTHLYPREMIUMTOENDJAN2012 <- as.numeric(gsub(" ","",All_Data$OLDMONTHLYPREMIUMTOENDJAN2012))
+# All_Data$OLDMONTHLYPREMIUMTOENDJAN2012[is.na(All_Data$OLDMONTHLYPREMIUMTOENDJAN2012)] = 0
+# All_Data$OLDMONTHLYPREMIUMTOENDJAN2013 <- as.numeric(gsub(" ","",All_Data$OLDMONTHLYPREMIUMTOENDJAN2013))
+# All_Data$OLDMONTHLYPREMIUMTOENDJAN2013[is.na(All_Data$OLDMONTHLYPREMIUMTOENDJAN2013)] = 0
+# All_Data$OLDMONTHLYPREMIUMTOENDJAN2014 <- as.numeric(gsub(" ","",All_Data$OLDMONTHLYPREMIUMTOENDJAN2014))
+# All_Data$OLDMONTHLYPREMIUMTOENDJAN2014[is.na(All_Data$OLDMONTHLYPREMIUMTOENDJAN2014)] = 0
+# All_Data$OLDMONTHLYPREMIUMTOENDJAN2015 <- as.numeric(gsub(" ","",All_Data$OLDMONTHLYPREMIUMTOENDJAN2015))
+# All_Data$OLDMONTHLYPREMIUMTOENDJAN2015[is.na(All_Data$OLDMONTHLYPREMIUMTOENDJAN2015)] = 0
+# All_Data$OLDMONTHLYPREMIUMTOENDDEC15GAPJAN16MPW <- as.numeric(gsub(" ","",All_Data$OLDMONTHLYPREMIUMTOENDDEC15GAPJAN16MPW))
+# All_Data$OLDMONTHLYPREMIUMTOENDDEC15GAPJAN16MPW[is.na(All_Data$OLDMONTHLYPREMIUMTOENDDEC15GAPJAN16MPW)] = 0
+
+# Remove rows of NAs
+All_Data <- All_Data[!is.na(All_Data$POLICYNUMBER),]
+
+rm(Columns, gggg)
+
+
+# Create an original copy of the All_Data
+
+All_Data_Original <- All_Data
---
+++ R/Report.R
@@ -0,0 +1,88 @@
+# Create Lapse Report
+
+# Create new All_Data matrix depending on the criteria specified in the home page
+All_Data <- All_Data_Original
+
+source(paste(Path, "/R_Code/OpenDB.R", sep = ""))
+
+Active_Agents_Data <- dbReadTable(my_new_db, "agents")
+
+if (toupper(Agent) != "ALL") {All_Data <- All_Data[All_Data$SALESAGENT == Agent,]}
+
+
+
+
+## Create report matrix.
+
+no_col      <- month(DateEnd) - month(DateStart) + (year(DateEnd) - year(DateStart))*12
+no_row      <- elapsed_months(DateEnd, as.Date("2010-01-01"))
+report.df   <- data.frame(duration = 0:no_row)
+
+row_names <- c("NTU", 1:no_row)
+
+DATE <- DateStart
+col = 2
+row = 1
+
+while (col <= no_col){
+ 
+  # create column with heading as the month and year
+  report.df[,as.character(paste(year(DATE), month.abb[month(DATE)]))] <- NA
+  
+
+  # for row in 1:no_row
+  row = 1
+  while (row <= no_row + 1){
+    # lapsed = number of policies Commencement Date < DATE and new month(end date) - month(commencement date) = row
+
+    if (row == 1){
+      # Expsure for NTU is all policies which commenced before DATE
+      exposure <- sum(All_Data$COMMENCEMENTDATE <= DATE & !is.na(All_Data$NEW_END_DATE))
+      # The number of policies which NTUed are all policies which commenced before DATE whose Status is NTU
+      lapsed <- sum(All_Data$COMMENCEMENTDATE <= DATE &
+                                All_Data$STATUS == "NTU" &
+                                !is.na(All_Data$NEW_END_DATE))
+      report.df[row, col] = lapsed/exposure
+    } else {
+      # For non-NTU, the exposure is the number of policies which commenced before DATE and who ended after duration
+      # Since the number of months between policies which ended on the last day of the month is always one less than the duration,
+      #       we need to add one to the difference between the start and end date. We must then exclude all NTUs because NTUs would now have a duration of 1.
+      exposure <- sum(All_Data$COMMENCEMENTDATE <= DATE & 
+                                  elapsed_months(All_Data$NEW_END_DATE, All_Data$COMMENCEMENTDATE) + 1 >= report.df[row,1] &
+                                  elapsed_months(DATE, All_Data$COMMENCEMENTDATE) >= report.df[row,1] &
+                                  !is.na(All_Data$NEW_END_DATE) &
+                                  All_Data$STATUS != "NTU")
+      lapsed <- sum(All_Data$COMMENCEMENTDATE <= DATE &
+                                elapsed_months(All_Data$NEW_END_DATE, All_Data$COMMENCEMENTDATE) + 1 == report.df[row,1] &
+                                elapsed_months(DATE, All_Data$COMMENCEMENTDATE) >= report.df[row,1] &
+                                !is.na(All_Data$NEW_END_DATE) &
+                                All_Data$STATUS != "NTU")
+      report.df[row, col] = lapsed/exposure
+    }
+    
+    # report.df[row, col] = lapsed/exposure
+    row  <-  row + 1
+  }  
+  
+  # create a DATE variable for the column
+  if (month(DATE) == 12) {
+    DATE <- as.Date(paste(year(DATE)+1, "01", "01", sep="-"))
+  }else{
+    DATE <- as.Date(paste(year(DATE), month(DATE)+1, "01", sep="-"))
+  } 
+  
+col <- col+1  
+}
+
+
+report.df[1,1] <- "NTU"
+report.df <- subset(report.df, select = -duration)
+rownames(report.df) <- row_names
+
+rm(col, row, DATE, no_col, no_row, row_names)
+
+report.df[is.na(report.df)] <- ""
+if (All_Affinities == "YES" & length(Affinities) > 1) {Affinities <- c("not", Affinities)}
+
+dir.create(file.path(paste0(Path, "/Results"),Sys.Date()), showWarnings = F)
+write.csv(report.df, paste0(Path, "/Results/",Sys.Date(),"/", paste0(Affinities, collapse = " "),Groups, Voluntary," Groups ",Agent, " Agents.csv"))
